04d8850c3238ba5e59728787abab49a4a8e9cea5
Sanjana Mahesh <112317074+Sanjudikshai@users.noreply.github.com>
2026-01-22 19:19:57 +0530

    Time: 276 ms (33.73%), Space: 0B (100%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,12 @@
+-- select(
+-- select distinct(salary)
+-- from employee
+-- order by salary asc
+-- limit 1 offset 1) as SecondHighestSalary;
+
+select max(salary)
+as SecondHighestSalary 
+from employee 
+where salary<(
+select max(salary) 
+from employee);
