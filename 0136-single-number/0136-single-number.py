528680cd0fbb9741ea18d1045d090f0db41f8dbd
Sanjana Mahesh <112317074+Sanjudikshai@users.noreply.github.com>
2026-03-09 20:51:54 +0530

    Time: 3 ms (61.66%), Space: 21.7 MB (19.11%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,7 +1,16 @@
 class Solution:
     def singleNumber(self, nums):
-        result=0
-        for num in nums:
-            result=result^num
-        return result 
+        s=set()
+        for i in nums:
+            if i in s:
+                s.remove(i)
+            else:
+                s.add(i)
+        return s.pop()
+# class Solution:
+#     def singleNumber(self, nums):
+#         result=0
+#         for num in nums:
+#             result=result^num
+#         return result 
           
