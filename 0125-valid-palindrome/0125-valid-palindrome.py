1f35d453812abc241036d9df19167338a082b5a6
Sanjana Mahesh <112317074+Sanjudikshai@users.noreply.github.com>
2026-03-26 00:21:53 +0530

    Time: 3 ms (98.94%), Space: 19.6 MB (70.16%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        palindrome=''
+        for c in s :
+            if c.isalnum():
+                palindrome +=c.lower()
+        return palindrome == palindrome[::-1]
