a522df2b57b129535849fe005a1d55801a935a77
Sanjana Mahesh <112317074+Sanjudikshai@users.noreply.github.com>
2026-03-25 16:20:19 +0530

    Time: 0 ms (100%), Space: 19.1 MB (96.09%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,12 @@
+val = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+rom = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        ans = ""
+        for i in range(13):
+            while num >= val[i]:
+                ans += rom[i]
+                num -= val[i]
+        return ans
+
+        
