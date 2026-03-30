c464ba516f0ccb570a67c2dc19638db673ec1927
Sanjana Mahesh <112317074+Sanjudikshai@users.noreply.github.com>
2026-03-30 22:42:59 +0530

    Time: 0 ms (100%), Space: 19.4 MB (93.77%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -2,12 +2,12 @@ class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         res=[]
-        def sub(i,path):
-            res.append(path)
+        def sub(i,subset):
+            res.append(subset)
             for j in range(i,len(nums)):
                 if j>i and nums[j]==nums[j-1]:
                     continue
-                sub(j+1,path+[nums[j]])
+                sub(j+1,subset+[nums[j]])
         
         sub(0,[])
         return res
